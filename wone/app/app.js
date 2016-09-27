ee5dc658aa74e5e5c0030c9cfeb38b2768f3622f
Leonardo Saturnino <Leo@Leonardos-Mac-mini.local>
2016-09-27 18:08:21 -0300

    layout and code updates

--- wone/app/app.js
+++ wone/app/app.js
@@ -2,8 +2,10 @@ var application = require("application");
 application.mainModule = "views/main/main";
 application.cssFile = "./app.css";
 
-global.APP_ID = "jh6l7mvc6x6g8966";
-global.API_MASTER = "sCd6pXEfML7DQem3TMXDPfE4pfssdMOq";
+//global.APP_ID = "jh6l7mvc6x6g8966";
+//global.API_MASTER = "sCd6pXEfML7DQem3TMXDPfE4pfssdMOq";
+global.APP_ID = "ctg3ndoil7bqx34x";
+global.API_MASTER = "wzXJ1BiiLKhCYnJb56qUiEquH3h2HaaN";
 global.BS_SCHEME = "http";
 global.EVERLIVE = null;
 global.EVERLIVEQ = null;
